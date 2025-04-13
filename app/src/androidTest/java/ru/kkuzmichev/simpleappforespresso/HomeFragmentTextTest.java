ea13e0b0e80f667cef4ab92572c0001bd3f6e1a1
Aizhamal <aijashka79@gmail.com>
2025-04-13 18:29:56 +0600

    allure results

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/HomeFragmentTextTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/HomeFragmentTextTest.java
@@ -10,17 +10,25 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.runner.RunWith;
 
+import io.qameta.allure.kotlin.Allure;
+import io.qameta.allure.kotlin.junit4.AllureJunit4;
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@RunWith(AllureAndroidJUnit4.class)
 public class HomeFragmentTextTest {
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
-
     @Test
-    public void testHomeFragmentText() {
+    public void HomeFragmentText() {
+        Allure.step("Находим элемент с текстом");
         ViewInteraction mainText = onView(withId(R.id.text_home));
+        Allure.step("Проверяем, что текст совпадает");
         mainText.check(matches(withText("This is home fragment")));
     }
 }
