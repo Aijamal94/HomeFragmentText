8c6d817f8b621f01a3743c311ed4f930a436f8d4
Aizhamal <aijashka79@gmail.com>
2025-04-23 13:43:42 +0600

    Добавлен Intent URL

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/IntentTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/IntentTest.java
@@ -3,17 +3,23 @@ package ru.kkuzmichev.simpleappforespresso;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
+import android.content.Intent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -53,7 +59,12 @@ public class IntentTest {
                                         0),
                                 0),
                         isDisplayed()));
+        materialTextView.check(matches(withText("Settings")));
+        Intents.init();
         materialTextView.perform(click());
+        intended(hasData("https://google.com"));
+        intended(hasAction(Intent.ACTION_VIEW));
+        Intents.release();
     }
 
     private static Matcher<View> childAtPosition(
